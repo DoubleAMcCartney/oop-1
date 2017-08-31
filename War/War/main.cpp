18e53f7f996ec39741dc821d5fe19d6dff225080
DoubleAMcCartney <amm343@zips.uakron.edu>
2017-08-31 14:33:18 -0400

    filled in more code in main

--- War/War/main.cpp
+++ War/War/main.cpp
@@ -2,12 +2,31 @@
 
 #include <iostream>
 
-class card {
+enum suits {diamond, heart, club, spade};
 
+class card {
+	public:
+		card(int, suits); //rank, suit
+		int getRank();
+		suits getSuit();
+	protected:
+		int rank;
+		suits suit;
+	
 };
 
-class deck {
+//create card
+card::card(int r, suits s){
+	rank = r;
+	suits = s;
+}
 
+class deck {
+	public:
+		void shuffle();
+		card deal();
+	protected:
+		card deck[52];
 };
 
 class player {
